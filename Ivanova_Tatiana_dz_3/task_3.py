82ea267418b5e96c3f78f93dd3e7381b1f831002
Tatiana Ivanova <tatiana@24proweb.ru>
2020-02-13 11:54:20 +0700

    Lesson 03

---
+++ Ivanova_Tatiana_dz_3/task_3.py
@@ -0,0 +1,49 @@
+"""
+3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов.
+"""
+
+
+# Вариант 1
+def my_summ_2_max(a, b, c):
+    """
+    Возвращает сумму наибольших двух аргументов.
+
+    :param int|float a: число
+    :param int|float b: число
+    :param int|float c: число
+    :return:
+    """
+    data = list(vars().values())  # data = [a, b, c]
+    data.remove(min(data))
+    return sum(data)
+
+
+print(my_summ_2_max(5, 10, 5))
+print(my_summ_2_max(1, 2, 3))
+print('-' * 80)
+
+
+# Вариант 2
+def my_summ_2_max_f(a, b, c):
+    """
+    Возвращает сумму наибольших двух параметров, при условии что все параметры различны.
+
+    Исключения:
+
+    Вернет 0, если все параметры равны.
+
+    Если два параметра будут соответствовать минимальному значению, то будет возвращено значение максимального.
+
+    :param int|float a: число
+    :param int|float b: число
+    :param int|float c: число
+    :return:
+    """
+    data = [a, b, c]
+    return sum(filter(lambda x: x > min(data), data))
+
+
+print(my_summ_2_max_f(5, 10, 15))
+print(my_summ_2_max_f(5, 5, 5))
+print(my_summ_2_max_f(5, 5, 20))
